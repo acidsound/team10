9104077a19c9540980247b18c28a53b3fd5f1743
MiaeKim <beautiul.mimikim@gmail.com>
2014-05-30 02:24:16 +0900

    change font, class list

--- slidetest.js
+++ slidetest.js
@@ -13,7 +13,8 @@ Meteor.methods({
     }, {
       $push:{
         "chats":{
-          "message":params.message
+          "message":params.message,
+          "currpage":params.currpage
         }
       }
     });
@@ -33,13 +34,14 @@ if (Meteor.isServer) {
     Pointer.remove({});
   });
 
-  Meteor.publish('lists', function(slideId){
-    return Pointer.find({slideId:slideId});
+  Meteor.publish('lists', function(){
+    return Pointer.find();
   });
 }
 
 if (Meteor.isClient) {
   Meteor.startup( function () {
+      Meteor.subscribe('lists');
     $('#reveal-math').hide();
   $("#chatArea").hide();
    Reveal.initialize({slideNumber: true}); 
@@ -75,8 +77,6 @@ Template.slides.helpers({
     return currentPage;
   },
   register : function (slideId_) {
-    // register class at first
-    // Nessary
       Session.set('slideId', slideId_);
       Meteor.call('setSlideId', slideId_ );
       $("#slide_name").text(slideId_);
@@ -91,7 +91,6 @@ Template.slides.helpers({
   Template.slides.events({
     'click #class': function () {
       var slideId_ = $("#slideId").val();
-      Meteor.subscribe('lists', slideId_);
       Template.slides.register(slideId_);
     }
   });
@@ -105,10 +104,11 @@ Template.slides.helpers({
   Template.chats.events({
     'submit': function(e) {
       e.preventDefault();
+      var currentPage = ( Template.chats.Pointer() && Template.chats.Pointer().pagenum != undefined ) ? Template.chats.Pointer().pagenum : 0;
       Meteor.call('addChat', {
         slideId: Session.get('slideId'),
-        message: $("#chat").val()
-        // currpage : currentPagae
+        message: $("#chat").val(),
+        currpage : currentPage
       }, function(err,code) {
         console.log('addChat result', err, code);
         if (!err) {
@@ -122,9 +122,31 @@ Template.slides.helpers({
     }
   });
 
-  $(".list-name").click(function(){
-    Template.slides.register($(".list-name").text());
-  });
+  Template.lists.helpers({
+    Pointer : function () {
+      return Pointer.findOne({slideId: Session.get('slideId')});
+    },
+    register : function (slideId_) {
+        Session.set('slideId', slideId_);
+        Meteor.call('setSlideId', slideId_ );
+        $("#slide_name").text(slideId_);
+        var currentPage = ( Template.lists.Pointer() && Template.lists.Pointer().pagenum != undefined ) ? Template.lists.Pointer().pagenum : 0;
+        Reveal.slide( currentPage, 0, 0 );
+        $('#slidesmenu').hide();
+        $('#reveal-math').show();
 
+    }
+  })
+  Template.lists.events({
+    'click .list-name' : function(){
+      var slideId_ = $(".list-name").text();
+      Session.set('slideId', slideId_);
+      $("#slide_name").text(slideId_);
+      var currentPage = ( Template.lists.Pointer() && Template.lists.Pointer().pagenum != undefined ) ? Template.lists.Pointer().pagenum : 0;
+      Reveal.slide( currentPage, 0, 0 );
+      $('#slidesmenu').hide();
+      $('#reveal-math').show();
+    }
+  });
 }
 
